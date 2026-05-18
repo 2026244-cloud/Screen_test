3d7e438336f0c368c06c65756f20e765fa8bf2c4
2026244-cloud <2026244@student.cct.ie>
2026-05-18 12:28:36 +0100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -2,4 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Uber Pickups in New York City")
+st.title("Uber Pickups in NYC")
+
+DATE_COLUMNS="date/time"
+
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMNS] = pd.to_datetime(data[DATE_COLUMNS])
+    return data
+
+data_load_state = st.text('Loading data...')
+load_data(10000)
+data_load_state.text('Done!')
